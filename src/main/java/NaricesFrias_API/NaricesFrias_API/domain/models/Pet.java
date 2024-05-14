df6a427800902bef012c01f0f90ba11659355d8c
YCerezaY <ptc0028676@est.univalle.edu>
2024-05-13 21:21:51 -0400

    Create Pet.java

---
+++ src/main/java/NaricesFrias_API/NaricesFrias_API/domain/models/Pet.java
@@ -0,0 +1,11 @@
+package NaricesFrias_API.NaricesFrias_API.domain.models;
+
+public class Pet {
+    public int id;
+    public String name;
+    public String breed;
+    public String gender;
+    public String info;
+    public short status;
+    public int owner_id;
+}
