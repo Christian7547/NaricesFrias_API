cd9712365fa94b92ea4be017c35335a8910c0b3a
Christian7547 <eliasgonzalesec@gmail.com>
2024-05-16 22:51:10 -0400

    Cors Enable

--- src/main/java/NaricesFrias_API/NaricesFrias_API/NaricesFriasApiApplication.java
+++ src/main/java/NaricesFrias_API/NaricesFrias_API/NaricesFriasApiApplication.java
@@ -2,6 +2,9 @@ package NaricesFrias_API.NaricesFrias_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NaricesFriasApiApplication {
@@ -9,5 +12,4 @@ public class NaricesFriasApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NaricesFriasApiApplication.class, args);
 	}
-
 }
