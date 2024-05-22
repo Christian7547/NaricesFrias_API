e613a4bae77aac59d6f711b16703dcc97e2b59c1
YCerezaY <ptc0028676@est.univalle.edu>
2024-05-22 08:21:18 -0400

    Encrypt Password

---
+++ src/main/java/NaricesFrias_API/NaricesFrias_API/infrastructure/adapters/implementation/Encrypt.java
@@ -0,0 +1,29 @@
+package NaricesFrias_API.NaricesFrias_API.infrastructure.adapters.implementation;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encrypt {
+    public static String encrypt(String value) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(value.getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(encodedhash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
