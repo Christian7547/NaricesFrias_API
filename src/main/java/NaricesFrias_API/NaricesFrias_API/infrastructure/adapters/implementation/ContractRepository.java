c033381b85f748bc3cabbf7c955a3faefc7fb174
YCerezaY <ptc0028676@est.univalle.edu>
2024-06-03 23:56:06 -0400

    Update ContractRepository.java

--- src/main/java/NaricesFrias_API/NaricesFrias_API/infrastructure/adapters/implementation/ContractRepository.java
+++ src/main/java/NaricesFrias_API/NaricesFrias_API/infrastructure/adapters/implementation/ContractRepository.java
@@ -38,7 +38,7 @@ public class ContractRepository implements IContractDomainRepository {
     public int uspContract(int v_pet_id, String v_contract_type, String v_price, String v_serial, int v_owner_id,
                            String v_due_date, String v_sub_total, String v_total, String v_subtotal_buy,
                            int v_collar_id){
-        //int value = 0;
+        int value = 0;
         StoredProcedureQuery procedureQuery = entityManager.createStoredProcedureQuery("uspContract");
         procedureQuery.registerStoredProcedureParameter("v_pet_id", Integer.class, ParameterMode.IN);
         procedureQuery.registerStoredProcedureParameter("v_contract_type", String.class, ParameterMode.IN);
@@ -50,7 +50,7 @@ public class ContractRepository implements IContractDomainRepository {
         procedureQuery.registerStoredProcedureParameter("v_total", String.class, ParameterMode.IN);
         procedureQuery.registerStoredProcedureParameter("v_subtotal_buy", String.class, ParameterMode.IN);
         procedureQuery.registerStoredProcedureParameter("v_collar_id", Integer.class, ParameterMode.IN);
-        //procedureQuery.registerStoredProcedureParameter("insert_count", Integer.class, ParameterMode.OUT);
+        procedureQuery.registerStoredProcedureParameter("insert_count", Integer.class, ParameterMode.OUT);
 
         procedureQuery.setParameter("v_pet_id", v_pet_id);
         procedureQuery.setParameter("v_contract_type", v_contract_type);
@@ -62,11 +62,11 @@ public class ContractRepository implements IContractDomainRepository {
         procedureQuery.setParameter("v_total", v_total);
         procedureQuery.setParameter("v_subtotal_buy", v_subtotal_buy);
         procedureQuery.setParameter("v_collar_id", v_collar_id);
-        //procedureQuery.setParameter("insert_count", value);
+        procedureQuery.setParameter("insert_count", value);
 
 
         procedureQuery.execute();
-        //var x = procedureQuery.getOutputParameterValue("insert_count");
-        return 1;   //Integer.parseInt(x.toString());
+        var x = procedureQuery.getOutputParameterValue("insert_count");
+        return  Integer.parseInt(x.toString());
     }
 }
